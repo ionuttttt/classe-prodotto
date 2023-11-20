99e61734057b236d36ed51902bf8e5f409a63d93
Ciuca.20107 <Ciuca.20107@LABI2-13PC07>
2023-11-20 09:31:44 +0100

    Aggiungere i file di progetto.

---
+++ classe prodotto/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe_prodotto
+{
+    internal class Program
+    {
+        class prodotto
+        {
+            protected string nome;
+            protected float prezzo;
+
+            public prodotto()
+            {
+                nome = "farro";
+                prezzo = 10;
+            }
+
+            public prodotto(string nome, float prezzo)
+            {
+                this.nome = nome;
+                this.prezzo = prezzo;
+            }
+            public void Scrivi()
+            {
+                Console.WriteLine(nome + " costa " + prezzo);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            prodotto prod=new prodotto();
+            prod.Scrivi();
+            prodotto prod1 = new prodotto("casa",50);
+            prod1.Scrivi();
+            Console.ReadLine();
+
+
+        }
+    }
+}
